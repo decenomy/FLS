58dd3f7a7706c751a42e31ee09518e1933b90a20
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2020-12-29 11:44:28 -0600

    ZNZ (c)

--- src/rpc/masternode.cpp
+++ src/rpc/masternode.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2015-2019 The PIVX developers
 // Copyright (c) 2020 The Flits developers
+// Copyright (c) 2020 The ZNZ developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
