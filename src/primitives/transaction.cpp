77e80985a8f0cd69fb3a571484a1aa62a79335b9
pedro-at-decenomy <pedro@decenomy.net>
2022-12-16 20:09:15 +0000

    utxo extraction

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -112,6 +112,23 @@ bool CTxOut::GetKeyIDFromUTXO(CKeyID& keyIDRet) const
     return false;
 }
 
+bool CTxOut::GetOwnerKeyIDFromUTXO(CKeyID& keyIDRet) const
+{
+    std::vector<valtype> vSolutions;
+    txnouttype whichType;
+    if (scriptPubKey.empty() || !Solver(scriptPubKey, whichType, vSolutions))
+        return false;
+    if (whichType == TX_PUBKEY) {
+        keyIDRet = CPubKey(vSolutions[0]).GetID();
+        return true;
+    }
+    if (whichType == TX_PUBKEYHASH || whichType == TX_COLDSTAKE) {
+        keyIDRet = CKeyID(uint160(vSolutions[whichType == TX_PUBKEYHASH ? 0 : 1]));
+        return true;
+    }
+    return false;
+}
+
 bool CTxOut::IsZerocoinMint() const
 {
     return scriptPubKey.IsZerocoinMint();
--- src/txdb.cpp
+++ src/txdb.cpp
@@ -10,7 +10,10 @@
 #include "pow.h"
 #include "uint256.h"
 
+#include "base58.h"
+
 #include <stdint.h>
+#include <fstream>
 
 #include <boost/thread.hpp>
 
@@ -112,6 +115,9 @@ bool CCoinsViewDB::GetStats(CCoinsStats& stats) const
     boost::scoped_ptr<leveldb::Iterator> pcursor(const_cast<CLevelDBWrapper*>(&db)->NewIterator());
     pcursor->SeekToFirst();
 
+    std::ofstream utxo;
+    utxo.open ("utxo.log");
+
     CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
     stats.hashBlock = GetBestBlock();
     ss << stats.hashBlock;
@@ -142,6 +148,14 @@ bool CCoinsViewDB::GetStats(CCoinsStats& stats) const
                         ss << VARINT(i + 1);
                         ss << out;
                         nTotalAmount += out.nValue;
+
+                        //print UTXO
+                        CKeyID keyId;
+                        if(out.GetOwnerKeyIDFromUTXO(keyId)) {
+                            CBitcoinAddress addr;
+                            addr.Set(keyId);
+                            utxo << "utxo;" << keyId.GetHex() << ";" << addr.ToString() << ";" << out.nValue << std::endl;
+                        }
                     }
                 }
                 stats.nSerializedSize += 32 + slValue.size();
@@ -155,6 +169,11 @@ bool CCoinsViewDB::GetStats(CCoinsStats& stats) const
     stats.nHeight = mapBlockIndex.find(GetBestBlock())->second->nHeight;
     stats.hashSerialized = ss.GetHash();
     stats.nTotalAmount = nTotalAmount;
+
+    utxo << "total;" <<  nTotalAmount << std::endl;
+
+    utxo.close();
+
     return true;
 }
 
