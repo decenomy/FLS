f086859485b7a77600cac0c7e76b9bba0c2a2389
pedro-at-decenomy <pedro@decenomy.net>
2024-11-27 15:02:36 +0000

    replace missing placeholders

--- build.sh
+++ build.sh
@@ -28,8 +28,8 @@ fi
 
 # Sets variables needed for the build
 TICKER=${TICKER:-"${GITHUB_REPO}"}
-UI_NAME=${UI_NAME:-"__Decenomy__"}
-BASE_NAME=${BASE_NAME:-"__decenomy__"}
+UI_NAME=${UI_NAME:-"Flits"}
+BASE_NAME=${BASE_NAME:-"flits"}
 
 # Sets the build environment variable
 #   0: The build will use the builder image available on docker hub
